37a56ae2c67b63100e852e40b100709ee5d81479
Alí Sinisterra <haidarconh@gmail.com>
2025-08-08 13:30:10 -0500

    update write command

--- src/logbook.py
+++ src/logbook.py
@@ -1,9 +1,12 @@
-import subcommands.config, subcommands.write
+import subcommands.config
 import typer
-
 from rich.console import Console
+
+from interfaces.db import writeLog, readLogs
+from utilities.timekeeping import getCurrentUTC, isoTimeString
 from typing_extensions import Annotated
-from typing import List, Tuple, Optional
+from typing import List
+
 
 console = Console()
 
@@ -17,8 +20,27 @@ def main(ctx: typer.Context):
     console.print("[blue]logbook[/blue]")
     console.print("Try running --help!")
 
+@app.command()
+def write(
+    body: str = typer.Argument(help="Text for your log"),
+    tag: Annotated[str|None, typer.Option("-t", "--tag", help="A tag for your log")] = None,
+):
+    now = isoTimeString(getCurrentUTC())
+    if tag is not None:
+        writeLog(body, timestamp=now, tag=tag)
+    else:
+        writeLog(body, timestamp=now)
+
+@app.command()
+def read(
+    amount: int = typer.Argument(10, help="The amount of logs to show"),
+    tag: Annotated[List[str]|None, typer.Option("-t", "--tag", help="A tag for your log")] = None,
+):
+    for log in readLogs():
+        console.print(log)
+    
+
 app.add_typer(subcommands.config.app, name="config")
-app.add_typer(subcommands.write.app, name="config")
 
 if __name__ == "__main__":
     app()
