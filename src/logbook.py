49d8bc0d09f0fe9dbcbdc0e23f78f9b63fd3b43a
Alí Sinisterra <haidarconh@gmail.com>
2025-08-09 13:57:41 -0500

    update display

--- src/logbook.py
+++ src/logbook.py
@@ -1,8 +1,12 @@
 import subcommands.config
 import typer
-from rich.console import Console
 
-from interfaces.db import writeLog, fetchLogs
+from rich.console import Console
+from rich.table import Table
+from rich.prompt import Confirm
+from rich import box
+
+from interfaces.db import writeLog, fetchLogs, deleteAllLogs, deleteLogsByTag
 from utilities.timekeeping import getCurrentUTC, isoTimeString
 from typing_extensions import Annotated
 from typing import List
@@ -32,12 +36,34 @@ def write(
         writeLog(body, timestamp=now)
 
 @app.command()
+def delete(
+    tags: Annotated[List[str]|None, typer.Option("-t", "--tag", help="A tag to delete items by")] = None,
+):
+    """Deletes logs in your logbook."""
+    if not tags: # IMPORTANT; update when new filter type
+        if Confirm.ask("[red]It looks like you're trying to delete every single log in your logbook.[/red] Are you sure?", default=False):
+            deleteAllLogs()
+    elif tags:
+        for tag in tags:
+            deleteLogsByTag(tag)
+        console.print("Done!")
+
+@app.command()
 def read(
     amount: int = typer.Argument(10, help="The amount of logs to show"),
     tag: Annotated[List[str]|None, typer.Option("-t", "--tag", help="A tag for your log")] = None,
 ):
-    for log in fetchLogs():
-        console.print(log)
+    logs = fetchLogs()
+    table = Table(box=box.ROUNDED)
+
+    table.add_column("Timestamp", justify="center", style="bright_yellow", no_wrap=True)
+    table.add_column("Text", style="bright_green", no_wrap=False)
+    table.add_column("Tag", justify="center")
+
+    for log in logs:
+        table.add_row(log['timestamp'], f'"{log['body']}"', log['tag'])
+    
+    console.print(table)
     
 
 app.add_typer(subcommands.config.app, name="config")
