5b11e1b6c4cc92fac53aea9d6444e60962f3e600
Alí Sinisterra <haidarconh@gmail.com>
2025-08-09 15:12:43 -0500

    update tag config flow

--- src/subcommands/config.py
+++ src/subcommands/config.py
@@ -1,46 +1,60 @@
-from interfaces.db import createTag, changeLogTags, deleteLogsByTag, fetchLogsObject
+from interfaces.db import createTagDefinition, findTagDefinitions, deleteTagDefinition, findLogs, changeLogTags
 from interfaces.db import resetAppFolder
-
+import utilities.display as display
 from typing_extensions import Annotated
 
 import typer
 from rich.console import Console
-from rich import print
 
 console = Console()
 
-app = typer.Typer()
+app = typer.Typer(help="Configure your logbook and other data.")
 
 @app.command()
 def tag(
     action: str = typer.Argument(help="An action to take (create/delete)"), 
     tag_name: str = typer.Argument(help="A tag name"), 
 ):
+    """
+    Configure tags.
+    """
     if action == 'create':
-        createTag(tag_name, 'static')
+        createTagDefinition(tag_name, 'static')
 
     if action == 'delete':
-        logsObject = fetchLogsObject()
-        matchingEntries = [entry for entry in logsObject.find(tag=tag_name)] # pyright: ignore[reportOptionalMemberAccess]
+        if len(findTagDefinitions(name = tag_name)) == 0:
+            raise typer.Exit()
 
-        if len(matchingEntries) != 0:
-            if typer.confirm("This will update all logs related this tag. Are you sure?", default=True):
-                if typer.confirm("Would you like to change them to use a different tag?", default=True):
-                    new_tag = typer.prompt("Please input another tag to use")
-                    changeLogTags(new_tag=new_tag, old_tag=tag_name)
-                else:
-                    if typer.confirm("All logs related to this tag will be deleted. Are you sure?", default=False):
-                        deleteLogsByTag(tag_name)
+        if len(findLogs(tag=tag_name)) != 0:
+            if display.confirm("[bold red]This will update all logs related to this tag.[/bold red] Are you sure?", default=True) is False:
+                raise typer.Exit()
 
-            console.print("Deleted the tag tagName") # deleteTag(tag_name)
+            if display.confirm("Would you like to update related logs to use a different tag?", default=True):
+                new_tag_exists = False
+                while new_tag_exists is False:
+                    new_tag = display.prompt("Please input another tag to use", fade=True) or ""
+                    if len(findTagDefinitions(name=new_tag)) > 0 and new_tag != tag_name:
+                        changeLogTags(new_tag=new_tag, old_tag=tag_name)
+                        new_tag_exists = True
+                    else:
+                        console.print("[blue]It looks like that tag doesn't exist.[/blue]")
+            else:
+                if display.confirm("[bold red]All logs related to this tag will be deleted.[/bold red] Are you sure?", default=False) is False:
+                    raise typer.Exit()
         else:
-            if typer.confirm("Are you sure you'd like to delete this tag?", default=True) is True:
-                console.print("Deleted the tag tagName") # deleteTag(tag_name)
+            if typer.confirm("Are you sure you'd like to delete this tag?", default=True) is False:
+                raise typer.Exit()
+        
+        deleteTagDefinition(tag_name)
+        console.print("Deleted the tag tagName")
 
 @app.command()
 def data(
     action: str = typer.Argument(help="An action to take (reset)"), 
 ):
+    """
+    Manage your app data.
+    """
     if action == 'reset':
         if typer.confirm("This will reset everything in your data folder, including your preferences and logbook. Are you sure?", default=False):
             resetAppFolder()
