d7883d43960b31cba558d25bcee2c601fa62d041
Alí Sinisterra <haidarconh@gmail.com>
2025-08-10 12:04:52 -0500

    add progress bars!

--- src/subcommands/read.py
+++ src/subcommands/read.py
@@ -3,13 +3,14 @@ import typer
 
 from rich.console import Console
 from rich.table import Table
-from rich.prompt import Confirm
 from rich import box
 
 from interfaces.db import fetchLogs, findLogs, fetchTagDefinitions
 import utilities.timekeeping as tk
 from typing_extensions import Annotated
 from typing import List
+
+from rich.progress import BarColumn, Progress, TextColumn
 
 console = Console()
 app = typer.Typer(help="Read your logbook.")
@@ -31,27 +32,38 @@ def logs(
     descending: Annotated[bool, typer.Option("-d/-a", "--descending/--ascending", help="Whether to order logs in descending order")] = True 
 ):
     """Read logs in your logbook."""
+    with Progress(
+        TextColumn("{task.description} [progress.percentage]"),
+        BarColumn(),
+        TextColumn("{task.completed}/{task.total} items ({task.percentage:>3.0f}%)"),
+        transient=True
+    ) as progress:
+        logs = []
+        if tags is not None:
+            task_reading = progress.add_task("Finding your logs...", total=len(tags))
+            for tag in tags:
+                logs.extend(findLogs(tag=tag))
+                progress.advance(task_reading)
+        else:
+            task_reading = progress.add_task("Finding your logs...", total=1)
+            logs = fetchLogs()
+            progress.advance(task_reading)
 
-    logs = []
-    if tags is not None:
-        for tag in tags:
-            logs.extend(findLogs(tag=tag))
-    else:
-        logs = fetchLogs()
+        outputLogs = sorted(logs, key=lambda log: log[sortingOption], reverse=descending)
+        outputLogs = outputLogs[:amount]
 
-    outputLogs = sorted(logs, key=lambda log: log[sortingOption], reverse=descending)
-    outputLogs = outputLogs[:amount]
+        table = Table(box=box.ROUNDED)
 
-    table = Table(box=box.ROUNDED)
+        table.add_column("Time (YYYY-MM-DD)", justify="center", no_wrap=True)
+        table.add_column("Text", justify="center", style="bright_green", no_wrap=False)
+        table.add_column("Tag", justify="center")
 
-    table.add_column("Time (YYYY-MM-DD)", justify="center", no_wrap=True)
-    table.add_column("Text", justify="center", style="bright_green", no_wrap=False)
-    table.add_column("Tag", justify="center")
-
-    for log in outputLogs:
-        datetime_local = tk.convertToTimeZone(tk.makeDatetime(log['timestamp']), tk.getLocalTimeZone())
-        time_local = tk.makeTimeString(datetime_local, format="%Y-%m-%d, %H:%M:%S")
-        table.add_row(time_local, f'"{log['body']}"', log['tag'])
+        task_processing = progress.add_task("Making a neat table...", total=len(outputLogs))
+        for log in outputLogs:
+            datetime_local = tk.convertToTimeZone(tk.makeDatetime(log['timestamp']), tk.getLocalTimeZone())
+            time_local = tk.makeTimeString(datetime_local, format="%Y-%m-%d, %H:%M:%S")
+            table.add_row(time_local, f'"{log["body"]}"', log["tag"])
+            progress.advance(task_processing)
     
     console.print(table)
 
