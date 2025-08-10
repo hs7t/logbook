f4a00330cb25f7d1bf10b2c6ba2230d528ea3a60
Alí Sinisterra <haidarconh@gmail.com>
2025-08-09 20:26:45 -0500

    add more timekeeping logic

--- src/utilities/timekeeping.py
+++ src/utilities/timekeeping.py
@@ -1,9 +1,24 @@
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from tzlocal import get_localzone 
 
 def getCurrentUTC():
     return datetime.now(tz=ZoneInfo("UTC"))
 
-def isoTimeString(UTCTimeString): 
-    """Turns an UTC datetime into an UTC time string"""
-    return UTCTimeString.strftime("%Y-%m-%dT%H:%M:%SZ")
+def getLocalTimeZone():
+    return get_localzone()
+
+def convertToTimeZone(datetime: datetime, timezone: ZoneInfo):
+    return datetime.astimezone(timezone)
+
+def makeTimeString(datetime: datetime, format: str):
+    """Makes a string from a datetime using a format"""
+    return datetime.strftime(format)
+
+def makeISOTimeString(UTCDatetime): 
+    """Turns an UTC datetime into an ISO time string"""
+    return makeTimeString(UTCDatetime, "%Y-%m-%dT%H:%M:%SZ")
+
+def makeDatetime(ISOTimeString):
+    """Turns an ISO timestring into a UTC datetime"""
+    return datetime.fromisoformat(ISOTimeString)
