413563c33ea9f60d2807115c5806c21e868752ec
Alí Sinisterra <haidarconh@gmail.com>
2025-08-10 10:40:38 -0500

    make notifs nicer

--- src/utilities/display.py
+++ src/utilities/display.py
@@ -4,25 +4,30 @@ from enum import Enum
 
 console = Console()
 
-def prompt(text, default=None, fade=False):
-    if fade is True:
-        text = f"[grey54]{text}[/grey54]"
-
-    response = Prompt.ask(text, default=default)
-    return response
-
-def confirm(text, default=False, fade = False):
-    if fade is True:
-        text = f"[grey54]{text}[/grey54]"
-
-    response = Confirm.ask(text, default=default)
-    return response
-
 class NotificationStyle(str, Enum):
     neutral = "blue",
     waffle = "grey54",
     warn = "bold yellow",
     assure = "green"
+
+
+def prompt(text, default=None, fade=False):
+    style = ""
+    if fade is True:
+        style = NotificationStyle.waffle
+    text = f"[{style}]{text}[{style}]"
+
+    response = Prompt.ask(text, default=default)
+    return response
+
+def confirm(text, default=False, fade = False):
+    style = ""
+    if fade is True:
+        style = NotificationStyle.waffle
+    text = f"[{style}]{text}[{style}]"
+
+    response = Confirm.ask(text, default=default)
+    return response
 
 def notify(text, style: NotificationStyle|str = NotificationStyle.neutral):
     console.print(f"[{style}]{text}[/{style}]")
